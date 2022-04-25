47a321ef78f77317ea6310ee8fb36b0b4966895c
Maximiliano Firtman <firtman@gmail.com>
2022-04-25 14:35:28 -0500

    Offers

---
+++ app/src/main/java/com/frontendmasters/coffeemasters/App.kt
@@ -0,0 +1,48 @@
+package com.frontendmasters.coffeemasters
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.frontendmasters.coffeemasters.ui.theme.CoffeeMastersTheme
+
+@Preview
+@Composable
+fun App_Preview() {
+    CoffeeMastersTheme() {
+        App()
+    }
+}
+
+@Composable
+fun App() {
+    Scaffold(
+        topBar = {
+            TopAppBar() {
+                AppTitle()
+            }
+        },
+        bottomBar = {}
+    ) {
+        OffersPage()
+    }
+}
+
+@Composable
+fun AppTitle() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Coffee Masters Logo")
+    }
+}
--- app/src/main/java/com/frontendmasters/coffeemasters/OffersPage.kt
+++ app/src/main/java/com/frontendmasters/coffeemasters/OffersPage.kt
@@ -2,12 +2,16 @@ package com.frontendmasters.coffeemasters
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -21,39 +25,58 @@ import androidx.compose.ui.unit.sp
 import com.frontendmasters.coffeemasters.ui.theme.Alternative1
 import com.frontendmasters.coffeemasters.ui.theme.Alternative2
 
-@Preview(showBackground = true, widthDp = 400)
+//@Preview(showBackground = true, widthDp = 400)
+//@Composable
+//private fun Offer_Preview() {
+//    Offer("My Title 1", description = "This is the description")
+//}
+
+@Preview(showBackground = true)
 @Composable
-fun Offer() {
-    val style = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-
-    Image(painter = painterResource(R.drawable.background_pattern),
-        contentDescription = "Background pattern",
-        contentScale = ContentScale.FillWidth,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(180.dp))
-
+fun OffersPage() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+        Modifier.verticalScroll(rememberScrollState())
+    ) {
+        Offer(title = "Early Coffe", description = "10% off. Offer valid from 6am to 9am.")
+        Offer(title = "Welcome Gift", description = "25% off on your first order.")
+        Offer(title = "Welcome Gift", description = "25% off on your first order.")
+    }
+}
+
+@Composable
+fun Offer(title: String, description: String = "") {
+    Box(
         modifier = Modifier.padding(16.dp)
     ) {
-        Text(text = "My Title",
-            style = MaterialTheme.typography.h5,
-            fontWeight = FontWeight.Bold,
-        modifier = Modifier
-            .background(Alternative2)
-            .padding(16.dp)
+        Image(painter = painterResource(id = R.drawable.background_pattern),
+            contentDescription = "Background Pattern",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier.matchParentSize()
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Description",
-            style = MaterialTheme.typography.h6,
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .background(Alternative2)
                 .padding(16.dp)
-        )
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .background(Alternative2)
+                    .padding(16.dp)
+            )
+            Text(
+                text = description,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .background(Alternative2)
+                    .padding(16.dp)
+            )
+
+        }
     }
+
 }
