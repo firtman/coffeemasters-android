e8f0fc1dba3975e19ab3e80a0e03a10e2744dcc5
Maximiliano Firtman <firtman@gmail.com>
2022-04-25 12:55:02 -0500

    Initial commit

---
+++ app/src/main/java/com/frontendmasters/coffeemasters/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.frontendmasters.coffeemasters
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.frontendmasters.coffeemasters.ui.theme.CoffeeMastersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoffeeMastersTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    FirstComposable()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstComposable() {
+    var name = remember { mutableStateOf("") }
+    
+    Column() {
+        Text("Hello ${name.value}",
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.Yellow)
+                .padding(16.dp)
+        )
+        TextField(value = name.value, onValueChange = {
+            name.value = it
+        })
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
